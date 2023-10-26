72f44ba1d128e1dcd6800472b360aa0f387933cd
haylo237 <njikeabednego@hotmail.com>
2023-10-26 02:36:46 +0100

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x = 0):
+    count = 0
+
+    for item in range(x):
+        try:
+            print("{}".format(my_list[item]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        print("")
+        return (count)
