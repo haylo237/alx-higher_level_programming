b6cd172fff90b4b0f15ea819e82f0738daf90269
haylo237 <njikeabednego@hotmail.com>
2023-10-26 03:46:03 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a class square that defines a square"""
+class Square(object):
+    """class square"""
+    def __init__(self, size):
+        """initialisd attributes"""
+        self.__size = size
