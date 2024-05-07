59bdb6f3104aad05dfae630e1ea9f67dd2624c3e
Njike Abednego <njikeabednego@hotmail.com>
2024-05-07 15:56:41 -0700

    task 5 and 6

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Define a class square"""
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square"""
+        self.size = size
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an interger")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+
+    def area(self):
+        return (self.__size * self.__size)
+
